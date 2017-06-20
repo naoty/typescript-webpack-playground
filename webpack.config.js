bd6c91ba9587db1b3e5965c3f3a2ea9c61c1dde2
Naoto Kaneko <naoty.k@gmail.com>
2017-06-20 16:06:07 +0900

    Setup webpack to bundle sources in TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,17 @@
 let path = require("path");
 
 module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      }
+    ]
+  }
 }
